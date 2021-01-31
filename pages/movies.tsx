ab912c65d8207f9dc56795b1b31aa06288de166b
Nytheneals <nythes@gmail.com>
2021-01-31 00:12:56 -0600

    WIP: hook the movie API

--- pages/movies.tsx
+++ pages/movies.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import { BASE_URL } from '../services';
 
-export default function Movies() {
-  return <div>Movies</div>;
+export default function Movies({ movies }) {
+  console.log('movies :>> ', movies);
+  return (
+    <div>
+      <pre>{JSON.stringify(movies, null, 2)}</pre>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const { API_KEY } = process.env;
+  const res = await fetch(
+    `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`,
+  );
+  const movies = await res.json();
+
+  return {
+    props: {
+      movies: movies.results,
+    },
+  };
 }
--- pages/movies/[id].tsx
+++ pages/movies/[id].tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 
+// import
+
 export default function movie() {
   const router = useRouter();
   const {
     query: { id },
   } = useRouter();
 
-  console.dir(router);
   return <div>Movieeeeee {id}</div>;
 }
