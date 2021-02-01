706b699981cad8704c16df425e39bbab6d0f11a1
nytheneals <nythes@gmail.com>
2021-02-01 16:02:53 -0600

    WIP: Hook up List Call

--- pages/movies.tsx
+++ pages/movies.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
-import { BASE_URL } from '../services';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
-export default function Movies({ movies }) {
-  console.log('movies :>> ', movies);
+// INTERFACES
+import { InferGetStaticPropsType } from 'next';
+import { Response } from '../interfaces/MovieList';
+
+const Movies = ({ movies }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  if (!movies) return null;
+  const { results } = movies;
+
   return (
-    <div>
-      <pre>{JSON.stringify(movies, null, 2)}</pre>
+    <div className={styles.grid}>
+      {results.map((movie) => {
+        const ellipsis = `${movie.overview.slice(0, 90)}...`;
+        return (
+          <Link key={movie.id} href={`movies/${movie.id}`}>
+            <a className={styles.card}>
+              <h3>{movie.title} &rarr;</h3>
+              <p className={styles.overview}>{ellipsis}</p>
+            </a>
+          </Link>
+        );
+      })}
     </div>
   );
-}
+};
 
-export async function getStaticProps() {
-  const { API_KEY } = process.env;
-  const res = await fetch(
-    `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`,
-  );
-  const movies = await res.json();
+export const getStaticProps = async () => {
+  const { API_KEY, BASE_URL } = process.env;
+  const URL = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
 
+  const res = await fetch(URL);
+  const movies: Response = await res.json();
   return {
     props: {
-      movies: movies.results,
+      movies,
     },
   };
-}
+};
+
+export default Movies;
