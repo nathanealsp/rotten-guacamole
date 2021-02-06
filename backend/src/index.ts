e9e60f0516247e649baef2edf88411dee13e0794
nytheneals <nythes@gmail.com>
2021-02-06 01:46:39 -0600

    WIP: Ports have some issues

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,6 +14,6 @@ const server = new ApolloServer({
 });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
